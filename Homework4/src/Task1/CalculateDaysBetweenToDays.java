38bc301c7450781e52da757f6677c542e814f74f
Solar0409 <149697890+Solar0409@users.noreply.github.com>
2024-01-14 20:32:09 +0300

    homeworks

---
+++ Homework4/src/Task1/CalculateDaysBetweenToDays.java
@@ -0,0 +1,17 @@
+package Task1;
+
+import java.time.Month;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class CalculateDaysBetweenToDays {
+    public static void main(String[] args) {
+        LocalDate dateBefore= LocalDate.of (2023,Month.DECEMBER,01);
+        LocalDate dateAfter = LocalDate.of(2024,Month.JANUARY,01);
+        long daysBetween = ChronoUnit.DAYS.between(dateBefore,dateAfter);
+        System.out.println(daysBetween);
+
+
+
+    }
+}
---
+++ Homework4/src/Task1/Validator.java
@@ -0,0 +1,5 @@
+package Task1;
+
+public interface Validator {
+    boolean validate(String input);
+}
