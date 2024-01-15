be4ecda9454664767837b020c73c3da8c7f8e5bb
Solar0409 <149697890+Solar0409@users.noreply.github.com>
2024-01-15 17:57:33 +0300

    Homework4

---
+++ Homework1/src/Task1/Main.java
@@ -0,0 +1,9 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = Customer.initializeCustomer();
+        double finalPrice = Shop.calculateFinalPrice(customer);
+        System.out.println("Финальная цена покупки: " + finalPrice);
+    }
+}
---
+++ Homework3/src/Cosmetics.java
@@ -0,0 +1,11 @@
+public class Cosmetics extends Product {
+
+    public Cosmetics (String name, double price, int quantity){
+        super(name, price, quantity);
+
+    }
+
+
+
+
+}
